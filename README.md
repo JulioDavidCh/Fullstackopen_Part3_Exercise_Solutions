4769e18e55321503b14a135dcc42f22fbfe84ef9
Julio <juliodavidchopite@gmail.com>
2019-12-06 11:58:40 -0300

    solutions exercise 3.1

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-The heroku application: https://rocky-refuge-57922.herokuapp.com/
-
-To get access to the data in the phonebooks, use this url: 
-https://rocky-refuge-57922.herokuapp.com/api/persons
-
-To get access to a single phone number, use this sintax:
-
-https://rocky-refuge-57922.herokuapp.com/api/persons/{"id of the number"}
-
-Example: https://rocky-refuge-57922.herokuapp.com/api/persons/2
-
-returns: 
-
-{
-name: "Ada Lovelace",
-number: "39-44-5323523",
-id: 2
-}
+Solution for the exercise 3.1 of the course
--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
-
-app.use(cors())
-
-app.use(bodyParser.json())
-
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    tokens.body(req,res)
-  ].join(' ')
-}))
 
 let persons =   [
   {
@@ -44,79 +24,11 @@ let persons =   [
   }
 ]
 
-//       ----------------ROUTES----------------
-
-app.get('/api/persons/:id', (req, res) =>{
-  const id = Number(req.params.id)
-  const phoneNumber = persons.find(personData => personData.id === id)
-
-  if(phoneNumber){
-    return res.json(phoneNumber)
-  }
-  res.status(404).end()
-})
-
-app.get('/api/persons', (req, res) =>{
-  res.json(persons) 
-})
-
-app.get('/info', (req, res) =>{
-  res.send(`
-  <div>
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${(new Date()).toString()}</p>
-  </div>
-  `)
-})
-
-app.delete('/api/persons/:id', (req, res) =>{
-  const id = Number(req.params.id)
-
-  persons = persons.filter(entry => entry.id !== id)
+  app.get('/api/persons', (req, res) =>{
+    res.json(persons) 
+  })
   
-  res.status(204).end()
-})
-
-const generateId = () =>{
-  const randomNumber = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
-  return randomNumber
-}
-
-app.post('/api/persons', (req, res) =>{
-  const body = req.body
-
-  if(!body.number){
-    //if request was sent without a number
-    return res.status(400).json({error: "phone number is mandatory, add one to your request"})
-  }else if(persons.find(entry => entry.name === body.name)){
-    //if name already exists in our phonebook
-    return res.status(400).json({error: 'name must be unique'})
-  }else if(!body.name){
-    //if request was sent without a name
-    return res.status(400).json({error: "name is mandatory, add one to your request"})
-  }
-
-  const id = generateId()
-
-  const newNumber = {
-    name: body.name,
-    number: body.number,
-    id: id
-  }
-
-  persons = [...persons, newNumber]
-
-  res.json(newNumber)
-})
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-  
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
